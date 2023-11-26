76431946f2694152cf42c35229450a9a4496945b
morsalinamunmun <morsalinamunmun@gmail.com>
2023-11-26 07:39:19 +0600

    teacher day section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,15 @@
 import Banner from "../../../components/Banner/Banner";
+import AddTeacher from "../AddTeacher/AddTeacher";
 import Partner from "../Partner/Partner";
+import UpcomingCourse from "../UpcomingCourse/UpcomingCourse";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Partner></Partner>
+            <UpcomingCourse></UpcomingCourse>
+            <AddTeacher></AddTeacher>
         </div>
     );
 };
---
+++ src/pages/Home/UpcomingCourse/UpcomingCourse.jsx
@@ -0,0 +1,13 @@
+import { FaGraduationCap } from "react-icons/fa6";
+const UpcomingCourse = () => {
+    return (
+        <div className="text-center">
+            <h2 className="uppercase font-bold text-3xl ">Upcoming Courses</h2>
+            <div className="flex items-center mx-auto">
+            <span className="border-2"></span><FaGraduationCap className="text-3xl "/><span className="border-2"></span>
+            </div>
+        </div>
+    );
+};
+
+export default UpcomingCourse;
